250b9a685d6d0f5127efed4b23aca509648cee6f
arihantXD <kamdearihant01@gmail.com>
2024-02-26 21:38:45 +0530

    updated ui 0.4

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,8 @@ function App() {
     <div className="max-w-[1600px] mx-auto body">
       {/* navbar styling */}
       <div className="flex text-[white] justify-between items-center bg-[#0F0F0F]  px-[20px] py-[10px] h-[70px]">
-        <span className="logo text-3xl text-[#FB6195]">AK</span>
-        <div className="flex text-sm gap-[30px]">
+        <span className="logo text-3xl md:text-4xl text-[#FB6195]">AK</span>
+        <div className="flex text-sm md:text-xl gap-[30px]">
           <span>About Me</span>
           <span>Projects</span>
           <span>Contact</span>
@@ -49,8 +49,8 @@ function App() {
           <h1>a MERN Stack Developer</h1>
           <h1>living in Nagpur</h1>
           <p className="mt-[50px]">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt,
-            soluta dolores, asperiores eligendi rerum excepturi.
+            👋 Hey! I am a MERN Stack Developer. I love working on exciting
+            projects and building modern and minimalistic web designs🚀.
           </p>
           <div className="mt-[70px] [&>button]:uppercase [&>button]:font-semibold [&>button]:py-[12px] [&>button]:px-[25px] [&>button]:rounded-xl flex flex-col sm:flex-row gap-[20px]">
             <button className="bgColor">Get in touch </button>
@@ -107,7 +107,7 @@ function App() {
       </div>
 
       {/* skill section !!!edit  */}
-      <div className="mt-[50px] h-[700px] md:h-[600px] px-[20px] min-h-[400px] flex flex-col gap-[50px] md:flex-row md:items-center justify-center">
+      <div className="mt-[50px] h-[700px]  px-[20px] min-h-[400px] flex flex-col gap-[50px] md:flex-row md:items-center justify-center">
         <div className="w-[100%] [&>h1]:text-3xl lg:[&>h1]:text-5xl lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
           <h1>Skills in my arsenal</h1>
           <div className="mt-[40px] p-[10px] flex gap-[20px] lg:gap-[40px] ">
@@ -220,7 +220,13 @@ function App() {
       </div>
 
       {/* projects section !!!edit  */}
-      <div className="px-[20px] mt-[130px] md:mt-[70px]">
+      <div
+        className={`${
+          (!front && !back) || (!add && !back) || (!front && !add)
+            ? "mt-[130px]"
+            : "mt-[40px]"
+        }  px-[20px] `}
+      >
         <div className="[&>h1]:text-3xl lg:[&>h1]:text-5xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
           <h1>My Projects</h1>
         </div>
@@ -297,7 +303,7 @@ function App() {
       <div className="mt-[50px] lg:mt-[70px] px-[20px] mx-auto  [&>h1]:text-3xl lg:[&>h1]:text-5xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
         <h1>Additional Projects</h1>
         <div className="md:flex md:gap-[5px] lg:gap-[25px]">
-          <div className="md:p-[10px] md:shadow-md mt-[30px]  [&>h1]:text-xl lg:[&>h1]:text-3xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
+          <div className="md:max-w-[33%] md:p-[10px] md:shadow-md mt-[30px]  [&>h1]:text-xl lg:[&>h1]:text-3xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
             <h1 className="">Weather.io 2.0</h1>
             <div className="sm:flex sm:flex-row-reverse text-sm md:text-base gap-[10px] md:flex-col ">
               <div>
@@ -307,16 +313,17 @@ function App() {
                   className="mt-[20px] sm:max-w-[200px] sm:min-h-[130px] md:min-w-[100%] mx-auto"
                 />
               </div>
-              <p className="mt-[20px]">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                laudantium eaque earum magnam! Eveniet rerum eum expedita dicta
-                iste repellendus.
+              <p className="mt-[20px] text-sm">
+                A sleek user-friendly weather application crafted with React.
+                Get real-time weather information for any location worldwide,
+                including current conditions and a 5-day forecast with an
+                intuitive graph that visualizes temperature trends.
               </p>
             </div>
           </div>
-          <div className="md:p-[10px] md:shadow-md mt-[30px]   [&>h1]:text-xl lg:[&>h1]:text-3xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
+          <div className="md:max-w-[33%] md:p-[10px] md:shadow-md mt-[30px]   [&>h1]:text-xl lg:[&>h1]:text-3xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
             <h1 className="">Weather.io 1.0</h1>
-            <div className="sm:flex sm:flex-row-reverse sm:text-sm gap-[10px] md:flex-col">
+            <div className=" sm:flex sm:flex-row-reverse sm:text-sm gap-[10px] md:flex-col">
               <div>
                 <img
                   src="/weatherOld.png"
@@ -324,14 +331,14 @@ function App() {
                   className="mt-[20px] sm:max-w-[200px] sm:min-h-[130px] md:min-w-[100%] mx-auto"
                 />
               </div>
-              <p className="mt-[20px]">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                laudantium eaque earum magnam! Eveniet rerum eum expedita dicta
-                iste repellendus.
+              <p className="mt-[20px] text-sm">
+                An environmentally conscious weather app crafted using vanilla
+                JavaScript, HTML, and CSS. This app focuses on air quality. Get
+                real-time data on pollutants, sunrise, and sunset times.
               </p>
             </div>
           </div>
-          <div className="md:p-[10px] md:shadow-md  mt-[30px]  [&>h1]:text-xl lg:[&>h1]:text-3xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
+          <div className="md:max-w-[33%] md:p-[10px] md:shadow-md  mt-[30px]  [&>h1]:text-xl lg:[&>h1]:text-3xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
             <h1 className="">Portfolio 1.0</h1>
             <div className="sm:flex sm:flex-row-reverse sm:text-sm gap-[10px] md:flex-col">
               <div>
@@ -341,10 +348,10 @@ function App() {
                   className="mt-[20px] sm:max-w-[200px] sm:min-h-[130px] md:min-w-[100%] mx-auto"
                 />
               </div>
-              <p className="mt-[20px]">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                laudantium eaque earum magnam! Eveniet rerum eum expedita dicta
-                iste repellendus.
+              <p className="mt-[20px] text-sm">
+                A dynamic showcase of my skills, crafted using HTML, CSS, and
+                JavaScript. Dive into my projects, and do checkout the magic of
+                GSAP animations.
               </p>
             </div>
           </div>
@@ -353,13 +360,13 @@ function App() {
 
       {/*  seperator  */}
       <div className="md:flex md:justify-evenly md:gap-[30px] md:items-center md:py-[50px]  w-[100%] mt-[50px] bg-[#0F0F0F] py-[40px]">
-        <div className="mt-[30px] md:m-0 flex items-center flex-col font-semibold">
+        <div className="mt-[40px] md:m-0 flex items-center flex-col font-semibold">
           <h2 className=" uppercase color leading-relaxed lg:leading-[70px] text-2xl lg:text-4xl">
             Projects Done
           </h2>
           <h1 className="text-white md:mt-[10px] text-xl lg:text-2xl">8+</h1>
         </div>
-        <div className="mt-[30px] md:m-0 flex items-center flex-col   font-semibold ">
+        <div className="mt-[40px] md:m-0 flex items-center flex-col   font-semibold ">
           <h2 className=" uppercase color leading-relaxed lg:leading-[70px] text-2xl lg:text-4xl">
             Experience
           </h2>
@@ -367,7 +374,7 @@ function App() {
             2 Years
           </h1>
         </div>
-        <div className="mt-[30px] mb-[10px] md:m-0 flex items-center flex-col   font-semibold ">
+        <div className="mt-[40px] mb-[10px] md:m-0 flex items-center flex-col   font-semibold ">
           <h2 className=" uppercase color leading-relaxed lg:leading-[70px] text-2xl lg:text-4xl">
             Certifications
           </h2>
@@ -376,25 +383,48 @@ function App() {
       </div>
 
       {/* footer and contacts  */}
-      <div className="px-[20px] mb-[20px] mt-[50px] md:mt-[70px] [&>h1]:text-2xl lg:[&>h1]:text-5xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
+      <div className="px-[20px] mb-[20px] mt-[50px] md:mt-[70px] [&>h1]:text-3xl lg:[&>h1]:text-5xl [&>h1]:leading-relaxed lg:[&>h1]:leading-[70px] [&>h1]:font-semibold">
         <h1>Contact Me</h1>
         <div>
-          <div className="flex flex-row items-center justify-between">
-            <div>
-              <h1 className="text-xl lg:text-3xl leading-relaxed lg:leading-[70px] font-light">
-                <span className="text-2xl lg:text-5xl leading-relaxed lg:leading-[70px] font-medium">
-                  A
-                </span>
-                rihant
-              </h1>
-            </div>
-            <div className="flex text-sm md:text-base font-light gap-[25px]">
-              <span>Home</span>
-              <span>Projects</span>
-              <span>My Skills</span>
+          <div className="">
+            <h1 className="text-xl lg:text-3xl leading-relaxed l7g:leading-[70px] font-light">
+              <span className="text-2xl md:text-4xl leading-relaxed lg:leading-[70px] font-medium">
+                A
+              </span>
+              rihant
+            </h1>
+          </div>
+          <div className="mt-[20px] py-[10px] flex flex-col items-center max-w-[800px] mx-auto shadow-md border-t-[2px] border-[pink]">
+            <span className="text-lg md:text-3xl leading-relaxed lg:leading-[70px] font-light text-center">
+              SEND ME A MESSAGE
+            </span>
+            <div className="min-w-[100%] mt-[5px] border-b-[2px] border-[pink] "></div>
+
+            <input
+              className="border-b-[2px] border-[#ddd] mt-[20px] w-[97%] max-w-[600px] px-[2px] text-[#676767] outline-none"
+              type="text"
+              name=""
+              id=""
+              placeholder="Ex. Arihant Kamde"
+            />
+            <input
+              className="border-b-[2px] text-[#676767] border-[#ddd] mt-[20px] w-[97%] px-[2px] max-w-[600px] placeholder:text-[#] outline-none"
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter your name"
+            />
+            <textarea
+              className="outline-none text-[#676767] mt-[20px] p-[2px] border-[2px] min-h-[150px]  w-[97%] max-w-[600px]"
+              placeholder="Enter your message"
+            ></textarea>
+            <div className="mt-[20px] [&>button]:uppercase [&>button]:font-bold [&>button]:py-[12px] [&>button]:px-[25px] [&>button]:rounded-xl flex flex-col sm:flex-row gap-[20px] ">
+              <button className="bg-[pink] w-[200px] text-[#333]">
+                Send Message
+              </button>
             </div>
           </div>
-          <div className="flex flex-col-reverse gap-[20px] justify-between md:flex-row md:gap-[200px]">
+          <div className="mt-[40px] flex flex-col-reverse gap-[20px] justify-between md:flex-row md:gap-[200px]">
             <p className="text-sm font-light">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Reprehenderit neque ut dolores inventore veritatis ullam in, ipsa
